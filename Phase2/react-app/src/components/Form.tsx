5c7de566bfb4e91c6a479c7ced3b576403c5da4f
toqitahmid <rubaiathali@gmail.com>
2026-09-13 20:42:31 +0600

    reacap react context api

--- Phase2/react-app/src/components/Form.tsx
+++ Phase2/react-app/src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useState, type FormEvent } from "react";
+import { useSharedState } from "../providers/counter.provider";
 
 type User = {
     id: number;
@@ -10,6 +11,7 @@ type FormProps = {
     setAllUsers: React.Dispatch<React.SetStateAction<User[]>>
 }
 export default function Form({ users, setAllUsers }: FormProps) {
+    const { count, setCount } = useSharedState();
     const [name, setName] = useState('');
     const [id, setId] = useState(0);
 
@@ -26,7 +28,13 @@ export default function Form({ users, setAllUsers }: FormProps) {
     }
     return (
         <div className='bg-fuchsia-100 rounded-2xl border-2 m-5 p-5'>
+            <div className="flex justify-center items-center text-4xl m-2 space-x-5">{count}
+                <button
+                    onClick={() => setCount(count + 1)}
+                    className='border-2 px-5 py-2 mt-3 rounded-2xl ml-5'>Incrimenet</button>
+            </div>
             <form className='' onSubmit={handleSubmit}>
+                
                 <div className='flex h-[30vh] justify-center items-center border-2 rounded-2xl'>
                     <p className='text-2xl'>Id</p>
                     <input type='text' className='border-2 h-10 rounded-2xl mr-3' onChange={(e) => setId(Number(e.target.value))} />
@@ -35,6 +43,7 @@ export default function Form({ users, setAllUsers }: FormProps) {
                 </div>
                 <div className='flex justify-center'>
                     <button className='border-2 px-5 py-2 mt-3 rounded-2xl' type='submit'>Submit</button>
+                    
                 </div>
             </form>
         </div>
--- Phase2/react-app/src/providers/counter.provider.tsx
+++ Phase2/react-app/src/providers/counter.provider.tsx
@@ -1,12 +1,26 @@
-import { createContext, useState } from "react"
+import { createContext, useContext, useMemo, useState } from "react"
 
-const CounterContext = createContext();
+interface CounterContextType {
+    count: number;
+    setCount: React.Dispatch<React.SetStateAction<number>>
+}
+
+const CounterContext = createContext<CounterContextType | null>(null);
 
 export default function CounterProvider({ children }) {
     const [count, setCount] = useState(0);
+    const contextValue = useMemo<CounterContextType>(() => ({count,setCount}),[count])
     return (
-        <CounterContext.Provider value={count, setCount}>
+        <CounterContext.Provider value={contextValue}>
             {children}
         </CounterContext.Provider>
     )
+}
+
+export function useSharedState() {
+    const context = useContext(CounterContext);
+    if (!context) {
+        throw new Error("useSharedState must be used within a CounterProvider");
+    }
+    return context;
 }
